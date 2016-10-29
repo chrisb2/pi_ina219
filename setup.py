1ff4c9fb97aedbb0fd449c5ff7fa6f14dd213baa
Chris Borrill <chris.borrill@gmail.com>
2016-10-29 19:24:28 +1300

    add module to setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ except ImportError:
     # Ignore import error and assume Python 3 which already has setuptools.
     pass
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 import sys
 
 # Define required packages.
 requires = ['Adafruit_GPIO', 'mock']
 
-setup(name              = 'ina219',
+setup(name              = 'pi-ina219',
       version           = '1.0.0',
       author            = 'Chris Borrill',
       author_email      = 'chris.borrill@gmail.com',
       description       = 'Library that supports the INA219 current and power monitor from Texas Instruments.',
-      license           = 'MIT',
+      license          = 'MIT',
       url               = 'https://github.com/chrisb/pi_ina219/',
       install_requires  = requires,
       test_suite        = 'tests',
-      packages          = find_packages())
+      pymodules         = ['ina219'])
