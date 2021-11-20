a90a0b560ba08d04b7acaedfb550e5a04b62720a
Chris Borrill <chris@borrill.gen.nz>
2021-11-20 18:43:30 +1300

    Fix issue with test dependency for smbus2

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'License :: OSI Approved :: MIT License',
                'Intended Audience :: Developers',
                'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3.4',
                'Programming Language :: Python :: 3.5',
                'Programming Language :: Python :: 3.6',
                'Programming Language :: Python :: 3.7',
+               'Programming Language :: Python :: 3.8',
                'Topic :: System :: Hardware :: Hardware Drivers']
 
 # Define required packages.
