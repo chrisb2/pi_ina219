d49cceeb350889811ea02e5c3bbd7a207bd30a60
Chris Borrill <chris@borrill.gen.nz>
2021-11-21 15:04:49 +1300

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'Programming Language :: Python :: 3.6',
                'Programming Language :: Python :: 3.7',
                'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9',
                'Topic :: System :: Hardware :: Hardware Drivers']
 
 # Define required packages.
